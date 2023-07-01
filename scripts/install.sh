8e9337e34209cf7d30e8c890558a57e9a251e350
ted-vo <vtnhon.dk@gmail.com>
2023-07-02 00:24:12 +0700

    feat: Makefile install

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,11 +1,60 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
-TOOLBOX_BIN_PATH="$(cd "$SCRIPT_DIR/.." && pwd)/bin"
+ROOT_DIR=$(cd "$SCRIPT_DIR/.." && pwd)
+ALACRITTY_PATH="$ROOT_DIR/alacritty"
+LOADING_PATH="$ROOT_DIR/shell-progress"
+TOOLBOX_BIN_PATH="$ROOT_DIR/bin"
+TMUX_PATH="$ROOT_DIR/tmux"
 
-echo "=== Add toolbox bin to PATH ==="
-cat >>~/.zshrc <<EOF
+# shellcheck disable=SC1091
+source "$LOADING_PATH/spinner.sh"
+
+zsh() {
+	start_spinner "  zsh"
+	sleep 0.1
+	stop_spinner $?
+}
+
+oh_my_zsh() {
+	start_spinner "  oh_my_zsh"
+	sleep 0.1
+	stop_spinner $?
+}
+
+alacritty() {
+	start_spinner "  alacritty"
+	test -d "$HOME/.config" || mkdir -p "$HOME/.config"
+	ln -Fs "$ALACRITTY_PATH" "$HOME/.config/alacritty"
+	sleep 0.1
+	stop_spinner $?
+}
+
+tmux() {
+	start_spinner "  tmux"
+	test -d "$HOME/.config" || mkdir -p "$HOME/.config/tmux"
+	ln -Fs "$TMUX_PATH" "$HOME/.config/tmux"
+	sleep 0.1
+	stop_spinner $?
+}
+
+toolbox() {
+	start_spinner "  toolbox/bin"
+	cat >>~/.zshrc <<EOF
 
 # toolbox/bin
 export PATH="$(printf '%s:$%s' "$TOOLBOX_BIN_PATH" "PATH")"
 EOF
+	sleep 0.1
+	stop_spinner $?
+}
+
+main() {
+	zsh
+	oh_my_zsh
+	alacritty
+	tmux
+	toolbox
+}
+
+main
