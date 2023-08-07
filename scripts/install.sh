c5641161d3b22d7925322f3b0b4a04a2263e7e1b
ted-vo <vtnhon.dk@gmail.com>
2023-08-07 17:33:00 +0700

    feat: scripts install

--- scripts/install.sh
+++ scripts/install.sh
@@ -26,37 +26,94 @@ alacritty() {
 	stop_spinner $?
 }
 
+gitconfig() {
+	start_spinner "  git"
+	ln -Fs "$ROOT_DIR/git/.gitconfig" "$HOME/.gitconfig"
+	sleep 0.1
+	stop_spinner $?
+}
+
 tmux() {
+	local tmux_path="$ROOT_DIR/tmux"
 	start_spinner "  oh-my-tmux"
-	test -d "oh-my-tmux" || git clone https://github.com/gpakosz/.tmux.git "$TMUX_PATH/oh-my-tmux" &>/dev/null
-	cd "$ROOT_DIR/tmux/oh-my-tmux" && git pull &>/dev/null
+	test -d "$tmux_path/oh-my-tmux" || git clone https://github.com/gpakosz/.tmux.git "$tmux_path/oh-my-tmux" &>/dev/null
+	cd "$tmux_path/oh-my-tmux" && git pull &>/dev/null
 
 	# test -d "$HOME/.config/tmux" || mkdir -p "$HOME/.config/tmux"
 	# ln -fs "$TMUX_PATH/oh-my-tmux/.tmux.conf" "$HOME/.config/tmux/tmux.conf"
 	# ln -fs "$TMUX_PATH/tmux.conf.local" "$HOME/.config/tmux/tmux.conf.local"
 
-	ln -Fs "$ROOT_DIR/tmux/oh-my-tmux/.tmux.conf" "$HOME/.tmux.conf"
-	ln -Fs "$ROOT_DIR/tmux/tmux.conf.local" "$HOME/.tmux.conf.local"
+	ln -Fs "$tmux_path/oh-my-tmux/.tmux.conf" "$HOME/.tmux.conf"
+	ln -Fs "$tmux_path/tmux.conf.local" "$HOME/.tmux.conf.local"
+	stop_spinner $?
+}
+
+nvim() {
+	start_spinner "  nvim"
+	test -d "$HOME/.config/nvim" || git clone https://github.com/ted-vo/nvim.git "$HOME/.config/nvim" &>/dev/null
+	cd "$HOME/.config/nvim" && git pull origin main &>/dev/null
 	stop_spinner $?
 }
 
 toolbox() {
 	start_spinner "  toolbox/bin"
-	cat >>~/.zshrc <<EOF
-
-# toolbox/bin
-export PATH="$(printf '%s:$%s' "$ROOT_DIR/bin" "PATH")"
+	cat >>~/.zshrc <<EOF # toolbox/bin
+export PATH="$(
+		printf '%s:$%s' "$ROOT_DIR/bin" "PATH"
+	)"
 EOF
 	sleep 0.1
 	stop_spinner $?
 }
-
-main() {
+all() {
 	zsh
 	oh_my_zsh
 	alacritty
+	gitconfig
 	tmux
+	nvim
 	toolbox
 }
 
-main
+main() {
+	case $1 in
+	zsh)
+		zsh
+		exit
+		;;
+	oh_my_zsh)
+		oh_my_zsh
+		exit
+		;;
+	alacritty)
+		alacritty
+		exit
+		;;
+	gitconfig)
+		gitconfig
+		exit
+		;;
+	tmux)
+		tmux
+		exit
+		;;
+	nvim)
+		nvim
+		exit
+		;;
+	toolbox)
+		toolbox
+		exit
+		;;
+	all)
+		all
+		exit
+		;;
+	*)
+		echo "Not support!"
+		exit
+		;;
+	esac
+}
+
+main $@
