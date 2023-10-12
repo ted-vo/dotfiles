f8f6c7582d8a5ea7ba0ea39911aac30d7e518247
ted-vo <vtnhon.dk@gmail.com>
2023-10-12 23:10:41 +0700

    bin: go-update

--- bin/go-update.sh
+++ bin/go-update.sh
@@ -1,16 +1,23 @@
 #!/usr/bin/env bash
 
-version=$(go tool dist version)
 release=($(wget -qO- "https://golang.org/VERSION?m=text"))
+release_version=${release[0]}
 
-if [[ "$version" == "${release[0]}"* ]]; then
-	echo "The local Go version ${release} is up-to-date."
-	exit 0
+if ! which go &>/dev/null; then
+	echo "Golang not install yet"
 else
-	echo "The local Go version is ${version}. A new release ${release} is available."
+	echo "Golang installed. Checking..."
+	version=$(go tool dist version)
+
+	if [[ "$version" == "$release_version"* ]]; then
+		echo "The local Go version ${release_version} is up-to-date."
+		exit 0
+	else
+		echo "The local Go version is ${version}. A new release ${release_version} is available."
+	fi
 fi
 
-release_file="${release}.linux-amd64.tar.gz"
+release_file="${release_version}.linux-amd64.tar.gz"
 
 tmp=$(mktemp -d)
 cd $tmp || exit 1
@@ -18,13 +25,14 @@ cd $tmp || exit 1
 echo "Downloading https://go.dev/dl/$release_file ..."
 curl -OL https://go.dev/dl/$release_file
 
-rm -f ${HOME}/apps/go 2>/dev/null
+[[ -d ${HOME}/apps ]] || mkdir ${HOME}/apps
+[[ -d ${HOME}/go ]] || rm -f ${HOME}/go 2>/dev/null
 
 tar -C ${HOME}/apps -xzf $release_file
 rm -rf $tmp
 
-mv ${HOME}/apps/go ${HOME}/apps/$release
-ln -sf ${HOME}/apps/$release ${HOME}/apps/go
+mv ${HOME}/apps/go ${HOME}/apps/$release_version
+ln -sf ${HOME}/apps/$release ${HOME}/go
 
 version=$(go tool dist version)
 echo "Now, local Go version is $version"
